f540ede1be5c43d883fe822e686bf726e3ae8a0f
Chamal1120 <kenway213beep@gmail.com>
2025-03-04 18:05:24 +0530

    feat: add initial pdf extraction method

--- lambda_function.py
+++ lambda_function.py
@@ -1,2 +1,17 @@
-print("hello from CV extractor")
+import pypdf
 
+
+def extract_text(file_path):
+    with open(file_path, "rb") as file:
+        reader = pypdf.PdfReader(file)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text() or ""
+    return text
+
+
+if __name__ == "__main__":
+    PDF_PATH = "Test-cv.pdf"
+    pdf_text = extract_text(PDF_PATH)
+    print("Raw PDF text:")
+    print(pdf_text)
